f1f7e3aa1351b77b13d1aa41d08f684f850f28af
Art Mendoza <artexmg@gmail.com>
2019-05-02 20:34:53 -0500

    .

--- code/JS/Sandbox/app.js
+++ code/JS/Sandbox/app.js
@@ -1,82 +1,27 @@
-// Thursday Review
-// 1: why does "null" is a primitive but shows as "object"? - google it!
+function addMessage(element, message) {
+  const messageElement = document.createElement("li");
+  messageElement.textContent = message;
+  element.appendChild(messageElement);
+}
 
-// 2: use of Wrappers to Convert datatypes / new Date(),
-console.log("------------- Wrappers for Datatypes -----------------");
+const first = document.getElementById("first");
+const second = document.getElementById("second");
 
-const numero = 123;
-const literal = "3.234";
-const bolean = false;
-
-let val = Number(numero);
-// val = String(numero);
-// val = Number(literal);
-// val = Number(literal);
-// val = parseFloat(literal);
-// val = parseInt(literal);
-// val = String(bolean);
-// val = Number(bolean);
-
-console.log(val + " is type " + typeof val);
-
-// 3: trick, how to get the last character of a string?
-
-console.log("------------- practice -----------------");
-const sentence = "This is the best day of Summer";
-console.log(sentence);
-//let lastChar = sentence.charAt(sentence.length - 1);
-//lastChar = sentence[sentence.length - 1];
-//console.log(lastChar);
-
-// 4: Array to string and
-//    and
-//    string to array (split)
-
-console.log("-------------Playing with arrays-----------------");
-const numeros = "1 2 3 4 5 6 7 8 9";
-const numArray = new Array(1, 2, 3, 4, 5, 6, 7, 8, 9);
-const newArray = numeros.split(" ");
-const languages = "english,french,portuguese,german,dutch,hindi";
-
-console.log(numeros);
-console.log(numArray);
-console.log(newArray);
-// console.log(newArray[1] + " " + newArray[2]);
-// console.log(numArray[1] + " " + numArray[2]);
-// console.log(String(numArray));
-// console.log(String(newArray));
-// console.log(String(newArray).split(","));
-// console.log(languages.split(","));
-
-// console.log(languages.replace("hindi", "talegu"));
-// console.log(languages.includes("japanesse"));
-// console.log(languages.includes("dutch"));
-
-val = numArray.length;
-val = Array.isArray(numArray);
-val = numArray.indexOf(1);
-
-// Extra - adding elements
-console.log("-------------Extra Arrays - Adding Elements -----------------");
-
-numArray.push(10);
-numArray.unshift(0);
-numArray.pop();
-val = numArray.shift();
-numArray.splice(1, 5);
-numArray.reverse();
-const subArray = new Array("elephant", "giraffe", "kohala");
-val = subArray.concat(numArray);
-
-console.log(val);
-console.log(numArray);
-
-//------------
-const today = new Date();
-let birthday = new Date("12/25/0");
-birthday = new Date("January 6 0001");
-
-val = today;
-val = birthday;
-
-console.log(val);
+addMessage(first, "Page Loading");
+// Catches "mousemove"
+document.body.addEventListener("mousemove", function() {
+  addMessage(second, "Event: mousemove");
+});
+// Catches "mousedown" event
+document.body.addEventListener("mousedown", function(e) {
+  if (e.button === 0) {
+    msg = "Left Click";
+  } else if (e.button === 1) {
+    msg = "Wheel click";
+  } else if (e.button === 2) {
+    msg = "Right Click";
+  } else {
+    msg = "Unknown button!";
+  }
+  addMessage(second, `Event: click ${msg}`);
+});
